32d68a4b203741d95526723457e1865f930c1972
彭钰程 <pengyucheng@weiyankeji.cn>
2023-10-12 09:55:56 +0800

    feat: 🚀 高级搜索搜索栏静态

---
+++ src/views/advancedSearch/components/advancedSearchBar.vue
@@ -0,0 +1,114 @@
+<template>
+	<div class="advanced-search-wrapper" :style="{ width: isCollapse ? '30px' : '224px' }">
+		<el-icon class="collapse-icon" @click="isCollapse = !isCollapse">
+			<component :is="isCollapse ? 'expand' : 'fold'"></component>
+		</el-icon>
+		<div :style="{ display: isCollapse ? 'none' : '' }">
+			<div class="component-title">
+				<el-icon><Search /></el-icon>
+				<span>高级搜索</span>
+			</div>
+			<el-input v-model="input1" size="large" placeholder="Please Input" prefix-icon="Search" />
+			<div>
+				<span class="sub-title">筛选条件</span>
+			</div>
+			<el-divider />
+			<div class="filter-criteria">
+				<span class="filter-title">数据源</span>
+				<el-select v-model="value" class="m-2" placeholder="Select" size="large">
+					<el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+				</el-select>
+				<el-select v-model="value" class="m-2" placeholder="Select" size="large">
+					<el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+				</el-select>
+				<el-divider border-style="dotted" />
+				<span class="filter-title">数据目录</span>
+				<el-select v-model="value" class="m-2" placeholder="Select" size="large">
+					<el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+				</el-select>
+				<span class="filter-title">数据标签</span>
+				<el-select v-model="value" class="m-2" placeholder="Select" size="large">
+					<el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+				</el-select>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isCollapse = ref(false);
+const input1 = ref("");
+
+const value = ref("");
+
+const options = [
+	{
+		value: "Option1",
+		label: "Option1",
+	},
+	{
+		value: "Option2",
+		label: "Option2",
+	},
+	{
+		value: "Option3",
+		label: "Option3",
+	},
+	{
+		value: "Option4",
+		label: "Option4",
+	},
+	{
+		value: "Option5",
+		label: "Option5",
+	},
+];
+</script>
+
+<style lang="scss" scoped>
+.advanced-search-wrapper {
+	overflow-x: hidden;
+	padding: 10px 12px;
+	background-color: #fff;
+	.collapse-icon.el-icon {
+		&:hover {
+			cursor: pointer;
+		}
+	}
+	.component-title {
+		display: flex;
+		align-items: center;
+		margin-bottom: 10px;
+		padding: 6px 4px;
+		border-radius: 4px;
+		background-color: #ccc;
+	}
+	.sub-title {
+		&::before {
+			display: inline-block;
+			margin-right: 5px;
+			border-radius: 2px;
+			width: 2px;
+			height: 12px;
+			background: #e46307;
+			content: "";
+		}
+	}
+	.filter-criteria {
+		display: flex;
+		flex-direction: column;
+		.filter-title {
+			margin-top: 10px;
+		}
+	}
+	.el-divider {
+		margin: 0;
+	}
+	.el-input,
+	.el-select {
+		margin-bottom: 14px;
+	}
+}
+</style>
--- src/views/advancedSearch/index.vue
+++ src/views/advancedSearch/index.vue
@@ -1,9 +1,15 @@
 <template>
-	<div class="">advancedSearch</div>
+	<div class="flex" style="height: 100%">
+		<AdvancedSearchBar />
+		<div class="result-wrapper"></div>
+	</div>
 </template>
 
 <script setup lang="ts">
 import {} from "vue";
+import AdvancedSearchBar from "./components/advancedSearchBar.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "./index";
+</style>
